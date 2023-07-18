8f86d4233ca4e935d5539a99ba507aa370e208bc
Da-fang Chang <dafangc@amazon.com>
2023-07-18 16:24:53 -0700

    Initial Commit

---
+++ app-1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  app-1
+//
+//  Created by Chang, Da-fang on 7/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ app-1/app_1App.swift
@@ -0,0 +1,17 @@
+//
+//  app_1App.swift
+//  app-1
+//
+//  Created by Chang, Da-fang on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct app_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
